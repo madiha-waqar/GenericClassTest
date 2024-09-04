8051eeaca55562f7b8ed4f22b88f62387f52d510
mrwiq <mrwiq@LT-MADIHA>
2024-09-03 20:42:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var collection = new ValueTypeCollection<int>();
+
+        collection.AddItem(5);
+        collection.AddItem(3);
+        collection.AddItem(10);
+        collection.AddItem(1);
+
+        int itemAtIndex1 = collection.GetItem(1);
+        Console.WriteLine($"Item at index 1: {itemAtIndex1}");
+
+        var sortedItems = collection.GetSortedDescending();
+        Console.WriteLine("Items in descending order:");
+        foreach (var item in sortedItems)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ ValueTypeCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ValueTypeCollection<T> where T : struct, IComparable<T>
+{
+     private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+     public T GetItem(int index)
+    {
+        if (index < 0 || index >= _items.Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+        return _items[index];
+    }
+
+     public List<T> GetSortedDescending()
+    {
+        return _items.OrderByDescending(item => item).ToList();
+    }
+}
